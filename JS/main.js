36e48e6252a660f2bc7ed02bbda26f8866c51cee
Mohamed Makawy <makawy390@gmail.com>
2022-03-25 14:31:13 +0200

    first commit

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+
+let toggle = document.querySelector('.toggle');
+
+toggle.onclick = function (){
+
+     toggle.classList.toggle('toggle-menu');
+
+     
+}
+
+// let list = document.querySelectorAll("header ul li");
+
+// list.onmouseenter = function () {
+//      list.classList.toggle("active");
+// }
